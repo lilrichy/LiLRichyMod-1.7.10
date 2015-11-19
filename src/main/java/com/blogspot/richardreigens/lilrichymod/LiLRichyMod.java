32e60b3578b17548383874b73e06c4f583f63f66
lilrichy <richardreigens@gmail.com>
2015-11-18 21:06:05 -0500

    Main Mod Class Created

---
+++ src/main/java/com/blogspot/richardreigens/lilrichymod/LiLRichyMod.java
@@ -0,0 +1,35 @@
+package com.blogspot.richardreigens.lilrichymod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Rich on 11/18/2015.
+ */
+
+@Mod(modid = "LiLRichyMod", name = "LiLRichyMod", version = "1.7.10-1.0", acceptedMinecraftVersions = "1.7.10")
+public class LiLRichyMod
+{
+    @Mod.Instance
+    public static LiLRichyMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
