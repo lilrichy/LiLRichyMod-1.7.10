a0814c758dad652886edfd618a0017f24d0842bc
lilrichy <richardreigens@gmail.com>
2015-11-25 19:38:22 -0500

    player detector side images working now

--- src/main/java/com/blogspot/richardreigens/lilrichymod/blocks/BlockPlayerDetector.java
+++ src/main/java/com/blogspot/richardreigens/lilrichymod/blocks/BlockPlayerDetector.java
@@ -3,10 +3,9 @@ package com.blogspot.richardreigens.lilrichymod.blocks;
 import com.blogspot.richardreigens.lilrichymod.reference.Names;
 import com.blogspot.richardreigens.lilrichymod.reference.Reference;
 import com.blogspot.richardreigens.lilrichymod.tileEntity.TileEntityPlayerDetector;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import io.netty.buffer.ByteBuf;
+import com.blogspot.richardreigens.lilrichymod.utility.LogHelper;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -14,19 +13,66 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * Created by Rich on 11/23/2015.
  */
 public class BlockPlayerDetector extends BlockTileEntityLiLRichyMod
 {
-    boolean activated;
-    private ByteBuf buf;
+    IIcon sideOff;
+    IIcon sideOn;
+    IIcon topBottom;
 
     public BlockPlayerDetector()
     {
         setBlockName(Names.Blocks.PLAYER_DETECTOR);
-        setBlockTextureName(Reference.MOD_ID.toLowerCase() + ":" + Names.Blocks.PLAYER_DETECTOR);
+        this.setHardness(3f);
+        this.setStepSound(soundTypeMetal);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        sideOff = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "playerDetectorOff");
+        sideOn = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "playerDetectorOn");
+        topBottom = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "playerDetectorTopBottom");
+    }
+
+    public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side)
+    {
+        TileEntityPlayerDetector te = (TileEntityPlayerDetector) world.getTileEntity(x, y, z);
+        ItemStack stack = te.getCamouflage();
+        if (stack != null && stack.getItem() instanceof ItemBlock) {
+            Block block = ((ItemBlock) stack.getItem()).field_150939_a;
+            return block.getIcon(side, stack.getItemDamage());
+        } else {
+            IIcon side_icon;
+            TileEntity tile = world.getTileEntity(x, y, z);
+            if (tile != null && te.getActivated())
+                side_icon = sideOn;
+            else
+                side_icon = sideOff;
+
+            LogHelper.info(te.activated + te.getActivated().toString());
+            if (side == 0)
+                return topBottom;
+            else if (side == 1)
+                return topBottom;
+            else
+                return side_icon;
+        }
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta)
+    {
+        if (side == 0)
+            return topBottom;
+        else if (side == 1)
+            return topBottom;
+        else
+            return sideOn;
     }
 
     @Override
@@ -38,30 +84,55 @@ public class BlockPlayerDetector extends BlockTileEntityLiLRichyMod
     @Override
     public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side)
     {
-        return !(side == 0 || side == 1);
+        return true;
     }
 
+    @Override
     public boolean canProvidePower()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isBlockSolid(IBlockAccess world, int x, int y, int z, int p_149747_5_)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isSideSolid(IBlockAccess world, int x, int y, int z, ForgeDirection side)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean hasTileEntity(int meta)
     {
         return true;
     }
 
     public int isProvidingWeakPower(IBlockAccess world, int x, int y, int z, int p_149709_5_)
     {
-        TileEntityPlayerDetector te = (TileEntityPlayerDetector) world.getTileEntity(x, y, z);
-        activated = te.getActivated();
-
-        if (activated == true) {
-            return 15;
-        } else
+        TileEntity te = world.getTileEntity(x, y, z);
+        TileEntityPlayerDetector detector = (te != null && te instanceof TileEntityPlayerDetector) ? (TileEntityPlayerDetector) te : null;
+        if (detector != null)
+            return detector.activated ? 15 : 0;
+        else
             return 0;
-
     }
 
+    @Override
+    public int isProvidingStrongPower(IBlockAccess world, int x, int y, int z, int meta)
+    {
+        TileEntity te = world.getTileEntity(x, y, z);
+        TileEntityPlayerDetector detector = (te != null && te instanceof TileEntityPlayerDetector) ? (TileEntityPlayerDetector) te : null;
+        if (detector != null)
+            return detector.activated ? 15 : 0;
+        else
+            return 0;
+    }
 
-
-
-
+    @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9)
     {
         if (!world.isRemote) {
@@ -76,16 +147,15 @@ public class BlockPlayerDetector extends BlockTileEntityLiLRichyMod
         return true;
     }
 
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side)
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta)
     {
-        TileEntityPlayerDetector te = (TileEntityPlayerDetector) world.getTileEntity(x, y, z);
-
-        ItemStack stack = te.getCamouflage();
-        if (stack != null && stack.getItem() instanceof ItemBlock) {
-            Block block = ((ItemBlock) stack.getItem()).field_150939_a;
-            return block.getIcon(side, stack.getItemDamage());
-        }
-        return super.getIcon(world, x, y, z, side);
+        super.breakBlock(world, x, y, z, block, meta);
+        world.notifyBlocksOfNeighborChange(x - 1, y, z, world.getBlock(x, y, z));
+        world.notifyBlocksOfNeighborChange(x + 1, y, z, world.getBlock(x, y, z));
+        world.notifyBlocksOfNeighborChange(x, y - 1, z, world.getBlock(x, y, z));
+        world.notifyBlocksOfNeighborChange(x, y + 1, z, world.getBlock(x, y, z));
+        world.notifyBlocksOfNeighborChange(x, y, z - 1, world.getBlock(x, y, z));
+        world.notifyBlocksOfNeighborChange(x, y, z + 1, world.getBlock(x, y, z));
     }
 }
--- src/main/java/com/blogspot/richardreigens/lilrichymod/tileEntity/TileEntityPlayerDetector.java
+++ src/main/java/com/blogspot/richardreigens/lilrichymod/tileEntity/TileEntityPlayerDetector.java
@@ -2,41 +2,41 @@ package com.blogspot.richardreigens.lilrichymod.tileEntity;
 
 import cpw.mods.fml.common.network.ByteBufUtils;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.AxisAlignedBB;
-
-import java.util.List;
 
 /**
  * Created by Rich on 11/23/2015.
  */
 public class TileEntityPlayerDetector extends TileEntityLiLRichyMod
 {
-    private boolean activated = false;
+    public boolean activated = false;
+    private int tick = 0;
+    private int rate = 5;
+    private int range = 5;
     private ItemStack camoStack;
     private NBTTagCompound tag = new NBTTagCompound();
-
 
     @Override
     public void updateEntity()
     {
-        if (!worldObj.isRemote) {
-            List<Entity> entities = worldObj.getEntitiesWithinAABB(EntityPlayer.class, AxisAlignedBB.getBoundingBox
-                    (xCoord - 1, yCoord - 1, zCoord - 1, xCoord + 2, yCoord + 2, zCoord + 2));
-            if (entities.size() > 0) {
-                activated = true;
-                worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
-                updateBlocks();
+        {
+            if (worldObj.isRemote)
+                return;
 
-            } else if (entities.size() < 1) {
-                activated = false;
-                worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
-                updateBlocks();
-
-            }
+            if (tick >= rate) {
+                tick = 0;
+                EntityPlayer player = worldObj.getClosestPlayer(xCoord + 0.5D, yCoord + 0.5D, zCoord + 0.5D, range + 0.5D);
+                if (player != null) {
+                    if (!activated)
+                        setActivated(true);
+                } else {
+                    if (activated)
+                        setActivated(false);
+                }
+            } else
+                tick++;
         }
     }
 
@@ -52,7 +52,15 @@ public class TileEntityPlayerDetector extends TileEntityLiLRichyMod
         worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord + 1, worldObj.getBlock(xCoord, yCoord, zCoord));
     }
 
+    public int getRange()
+    {
+        return range;
+    }
 
+    public void setRange(int value)
+    {
+        this.range = value;
+    }
 
     public ItemStack getCamouflage()
     {
@@ -70,38 +78,51 @@ public class TileEntityPlayerDetector extends TileEntityLiLRichyMod
         return activated;
     }
 
+    private void setActivated(boolean output)
+    {
+        activated = output;
+        worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+        updateBlocks();
+    }
+
     public void writeToPacket(ByteBuf buf)
     {
         ByteBufUtils.writeItemStack(buf, camoStack);
+        buf.writeBoolean(activated);
 
     }
 
     public void readFromPacket(ByteBuf buf)
     {
         camoStack = ByteBufUtils.readItemStack(buf);
+        activated = buf.readBoolean();
         worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+
     }
 
     @Override
     public void readFromNBT(NBTTagCompound tag)
     {
-        super.readFromNBT(tag);
+        activated = tag.getBoolean("activated");
+        range = tag.getInteger("range");
         if (tag.hasKey("camoStack")) {
             camoStack = ItemStack.loadItemStackFromNBT(tag.getCompoundTag("camoStack"));
         } else {
             camoStack = null;
         }
+        super.readFromNBT(tag);
     }
 
     @Override
     public void writeToNBT(NBTTagCompound tag)
     {
-        super.writeToNBT(tag);
-
+        tag.setBoolean("activated", activated);
+        tag.setInteger("range", range);
         if (camoStack != null) {
             NBTTagCompound t = new NBTTagCompound();
             camoStack.writeToNBT(t);
             tag.setTag("camoStack", t);
         }
+        super.writeToNBT(tag);
     }
 }
