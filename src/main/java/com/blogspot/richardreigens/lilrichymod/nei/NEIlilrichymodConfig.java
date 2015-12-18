7e85f578af0b0d1e63aa5efb122d7d3f86d6ae6c
lilrichy <richardreigens@gmail.com>
2015-12-17 21:53:01 -0500

    implemented the NEI and CCC API - what a pain in the arse

---
+++ src/main/java/com/blogspot/richardreigens/lilrichymod/nei/NEIlilrichymodConfig.java
@@ -0,0 +1,28 @@
+package com.blogspot.richardreigens.lilrichymod.nei;
+
+
+import codechicken.nei.api.IConfigureNEI;
+
+/**
+ * Created by Rich on 12/17/2015.
+ */
+public class NEIlilrichymodConfig implements IConfigureNEI
+{
+    @Override
+    public void loadConfig()
+    {
+
+    }
+
+    @Override
+    public String getName()
+    {
+        return null;
+    }
+
+    @Override
+    public String getVersion()
+    {
+        return null;
+    }
+}
